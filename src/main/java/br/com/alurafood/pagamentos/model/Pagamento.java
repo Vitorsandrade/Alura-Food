b2e767237811a18a6f3497db6ba5a3d12dff1360
Vitorsandrade <vitorsantos.dandrade@gmail.com>
2023-04-04 16:48:35 -0300

    config de banco

--- src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -25,16 +25,12 @@ public class Pagamento {
     @NotNull
     @Positive
     private BigDecimal valor;
-    @NotBlank
     @Size(max = 100)
     private String nome;
-    @NotBlank
     @Size(max = 19)
     private String numero;
-    @NotBlank
     @Size(max = 7)
-    private String expracao;
-    @NotBlank
+    private String expiracao;
     @Size(max = 3, min = 3)
     private String codigo;
     @Enumerated(EnumType.STRING)
