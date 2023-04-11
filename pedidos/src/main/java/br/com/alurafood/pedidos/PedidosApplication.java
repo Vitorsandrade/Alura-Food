974fe6ea5ff08e2ac9bbbc397548bbdf0870dbe9
Vitorsandrade <vitorsantos.dandrade@gmail.com>
2023-04-11 18:25:41 -0300

    balanceamento de carga

--- pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//anotação para se comunicar com eureka
 @EnableEurekaClient
 public class PedidosApplication {
 
